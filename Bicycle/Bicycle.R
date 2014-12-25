3eb79adb4b970b5d0180d554bc7bb5643688850c
Nielsj <niels.jansen@cashper.nl>
2014-12-25 18:22:22 +0100

    added heat maps

--- Bicycle/Bicycle.R
+++ Bicycle/Bicycle.R
@@ -18,7 +18,7 @@ library(dplyr)
 library(rpart)
 library(rpart.plot)
 library(rms)
-
+library(Metrics)
 train <- read.csv("~/Dropbox/Machine Learning/Kaggle/Bicycle/train.csv")
 test <- read.csv("~/Dropbox/Machine Learning/Kaggle/Bicycle/test.csv")
 
@@ -36,8 +36,13 @@ traintest <- rbind(train, test)
 
 #pull the weekday and hour from the datetime variable with R's strptime() function
 traintest$datetime <- strptime(traintest$datetime, format="%Y-%m-%d %H:%M:%S")
-traintest$weekday <- weekdays(traintest$datetime)
-traintest$hour <- traintest$datetime$hour
+#traintest$hour <- traintest$datetime$hour
+traintest$hour <- as.factor(substr(traintest$datetime, 12,13))
+traintest$weekday <- as.factor(weekdays(traintest$datetime))
+traintest$year <- as.factor(substr(traintest$datetime, 1,4))
+traintest$month <- as.factor(substr(traintest$datetime, 6,7))
+traintest$day <- as.factor(substr(traintest$datetime, 9,10))
+traintest$date <- as.Date(substr(traintest$datetime, 1,10))
         
 traintest$season <- as.factor(traintest$season)
 traintest$holiday <- as.factor(traintest$holiday)
@@ -49,40 +54,75 @@ train <- subset(traintest, dataset == "train")
 test <- subset(traintest,dataset == "test")
 
 #-END DATA MANIULATION-
-  
+
+#/START - DATA PLOTTING/
+
+# Save average counts for each day/time in data frame
+
+day_hour_counts <- as.data.frame(aggregate(train[,"count"], list(train$weekday, train$hour), mean))
+day_hour_counts$Group.1 <- factor(day_hour_counts$Group.1, ordered=TRUE, levels=c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+
+day_hour_counts$hour <- as.numeric(as.character(day_hour_counts$Group.2))
+
+# plot heat mat with ggplot
+library(ggplot2)
+ggplot(day_hour_counts, aes(x = hour, y = Group.1)) + geom_tile(aes(fill = x)) + scale_fill_gradient(name="Average Counts", low="white", high="green") + theme(axis.title.y = element_blank())
+
+
+
+
+
+
 #/START - MODELS/
+#define the formula for the fit
+formula <-count ~ 
+  season +
+  holiday +
+  workingday +
+  weather +
+  temp +
+  atemp +
+  humidity +
+  windspeed +
+  hour +
+  weekday
 
 #//START - AVERAGE AS THE PREDICTOR//
   
-train$predict_avg <- mean(train$count)
-MSE_Avg <- mean( (train$predict_avg - train$count)^2, na.rm = TRUE)
+train$predict.avg <- mean(train$count)
+
+RMSLE_AVG <- rmsle(train$count, abs(train$predict.avg))
 #--END AVERAGE AS THE PREDICTOR--
 
+#/START - CART TREE
+library(rpart)
 
-##START - CART TREE##
-
-fit <- rpart(count ~ 
-               season +
-               holiday +
-               workingday +
-               weather +
-               temp +
-               atemp +
-               humidity +
-               windspeed +
-               hour +
-               weekday
-              ,train,method="anova",model="true")
-printcp(fit) # display the results 
-plotcp(fit) # visualize cross-validation results 
-summary(fit) # detailed summary of splits
+fit.rpart <- rpart(formula,train)
+printcp(fit.rpart) # display the results 
+plotcp(fit.rpart) # visualize cross-validation results 
+summary(fit.rpart) # detailed summary of splits
 
 # plot tree, extra =2 -> number of correct/number of total observations
-prp(fit, faclen = 20, type = 3, varlen = 20)
+prp(fit.rpart, faclen = 20, type = 3, varlen = 20)
 
 #calculate the mse of the training set
-train$predict_cart <- predict(fit,train)
-MSE_CART <- mean( (train$predict_cart - train$count)^2, na.rm = TRUE)
+train$predict.rpart <- predict(fit.rpart,train)
+RMSLE_RPART <- rmsle(train$count, abs(train$predict.rpart))
+
+#--END - RPART
+
+##START - CART CTREE##
+library(party)
+library(partykit)
+
+fit.ctree <- ctree(formula,train,control =  ctree_control(maxdepth = "2"))
+plot(fit.ctree)
+fit.ctree
+
+#calculate the mse of the training set
+train$predict.ctree <- predict(fit.ctree,train)
+RMSLE_CTREE <- rmsle(train$count, abs(train$predict.ctree))
+
 #validate(fit)
 
 #--END - CART TREE--
@@ -92,24 +132,13 @@ MSE_CART <- mean( (train$predict_cart - train$count)^2, na.rm = TRUE)
 library(randomForest)
 
 #built a randomd Forest (for a random forest we first need to take care of missing values)
-fit_rf <- randomForest(count ~ 
-                         season +
-                         holiday +
-                         workingday +
-                         weather +
-                         temp +
-                         atemp +
-                         humidity +
-                         windspeed +
-                         hour +
-                         weekday
-                          , train, importance=TRUE, ntree=20, type='supervised' )
+fit.rf <- randomForest(formula, train, importance=TRUE, ntree=20, type='supervised' )
 #plot the importance of the variables of the random forest
-varImpPlot(fit_rf,sort=TRUE)
+varImpPlot(fit.rf,sort=TRUE)
 
 #calculate the mse of the training set
-train$predict_randomforest <- predict(fit_rf,train)
-MSE_RF <- mean( (train$predict_randomforest - train$count)^2, na.rm = TRUE)
+train$predict.rf <- predict(fit.rf,train)
+RMSLE_RF <- rmsle(train$count, abs(train$predict.rf))
 
 #--END - RANDOM FOREST--
 #-END - MODELS-
@@ -118,14 +147,15 @@ MSE_RF <- mean( (train$predict_randomforest - train$count)^2, na.rm = TRUE)
 #START - PREDICT#
 #for now we use the random forest to predict
   
-test$predict_count <- predict(fit_rf, test)
+test$predict.rf <- predict(fit.rf, test)
 
 #-END - PREDICT-
 
 #START - SUBMIT RESULTS#
 #write an output csv to submit to kaggle
 
-submit<-data.frame(datetime = test$datetime, count = round(test$predict_count,0))
+submit<-data.frame(datetime = test$datetime, count = round(test$predict.rf,0))
 write.csv(submit, file = "~/Dropbox/Machine Learning/Kaggle/Bicycle/submission.csv", row.names = FALSE)
 
 #-END - SUBMIT RESULTS-
+
